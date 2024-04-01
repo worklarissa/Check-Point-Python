e24c61859104d89764f5048f2d1194816cae00d5
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2024-04-01 16:42:09 -0300

    um método

--- proj-algoritmos/proj-algoritmos/algoritmos/ordenacao.py
+++ proj-algoritmos/proj-algoritmos/algoritmos/ordenacao.py
@@ -1,1 +1,25 @@
-# implemente aqui....
+# Aqui precisamos fazer as funções para busca ordenada ou chamar as funções 
+# prontas testar nas listas  que estão no arquivo de dados.
+
+class Ordenacao:
+  def bubble_sort(lista):
+    while #enquanto houver indices a  serem percorridos
+    n = len(lista)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j] ## troca de posição
+    return lista
+
+    def selection_sort(lista):
+        #entender o tamanho da minha lista
+        n = len(lista)
+        #encontrar o menor elemento e posicionar ele no primeiro indice
+        for i in range(n):
+            if i < n:
+                i = lista[0]
+            else:
+                continue
+        #percorrer para achar o próximo menor elemento
+        #percorrer a lista2 para encontrar o menor elemento
+        # adicionar na lista1     
